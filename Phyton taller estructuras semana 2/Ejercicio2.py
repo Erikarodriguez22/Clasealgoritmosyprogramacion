0085f5bc66229c779db0795c33cea4358e0f6709
Erika Rodríguez S <82105063+Erikarodriguez22@users.noreply.github.com>
2021-04-21 11:19:50 -0500

    Delete Phyton taller estructuras semana 2 directory

--- Phyton taller estructuras semana 2/Ejercicio2.py
+++
@@ -1,9 +0,0 @@
-"""
-Entradas
-Capital-->float-->Cap
-Salidas
-Ganancia-->float-->Gan
-"""
-Cap=(float(input("Digite el capital a invertir: ")))
-Gan=(Cap*0.02)
-print("La ganancia es: "+ str(Gan), "Ganancia con el capital invertido: "+ str(Gan+Cap))
